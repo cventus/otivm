df381a2b3dc72c349f80e7abd48f96b9e92fe282
Christoffer Ventus <christoffer.ventus@futurice.com>
2015-05-30 23:41:38 +0300

    Show one test result on one line
    Simplify output of run-tests.sh to show test binary and primary test
    results on one line. Only if verbose or additional information is
    specified on the command line will a test output more than one line.

--- ok/bin/run-tests.sh
+++ ok/bin/run-tests.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
+#
+# Run and parse each binary test provided on standard input
+#
+# Command line parameters are forwarded to tap.sh
 
-THISPATH=$(dirname "$(readlink -f "$0")")
+THISPATH=$(dirname "$0")
 
 while read test;
 do
-	echo $test
-	dir=$(dirname $test)/../out/
-	mkdir -p $dir
-	$test | tee $dir/$(basename $test).out | $THISPATH/tap.sh -ci
-	echo
+  echo -n "$test	"
+  OUT_DIR=$(dirname $(dirname $test))/out/
+  mkdir -p $OUT_DIR
+  if [ -e "$test" ]; then
+    $test | tee $OUT_DIR/$(basename $test).out | $THISPATH/tap.sh ${*:--c}
+  else
+    echo | $THISPATH/tap.sh ${*:--c}
+  fi
 done
 
--- ok/bin/tap.sh
+++ ok/bin/tap.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
+#
+# Parse a TAP stream and print a condensed report
 
 INFO=
 VERBOSE=
 COLOR=
 
-while getopts ivc o
-do	case "$o" in
-	i)	INFO="-v info=1";;
-	v)	VERBOSE="-v verbose=1";;
-	c)	COLOR="-v color=1";;
-	\?)	print >&2 "Usage: $0 [-i] [-v] [-c] file ..."
-		exit 1;;
-	esac
+while getopts ivc o; do
+  case "$o" in
+    i)  INFO="-v info=1";;
+    v)  VERBOSE="-v verbose=1";;
+    c)  COLOR="-v color=1";;
+   \?)  echo >&2 "Usage: $0 [-i] [-v] [-c] [file ...]";
+        exit 1;;
+  esac
 done
 shift `expr $OPTIND - 1`
 
-THISPATH=$(dirname "$(readlink -f "$0")")
+THISPATH=$(dirname "$0")
 
 sed -unf $THISPATH/parse-tap.sed $@ |
 awk $INFO $VERBOSE $COLOR -f $THISPATH/print-results.awk
