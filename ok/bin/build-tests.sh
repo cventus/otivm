ad8223db7d72e03cd825de3061541a0658e99050
Christoffer Ventus <christoffer.ventus@futurice.com>
2015-05-31 00:04:20 +0300

    Filter tests to build in build-tests.sh

--- ok/bin/build-tests.sh
+++ ok/bin/build-tests.sh
@@ -1,23 +1,47 @@
 #!/bin/sh
 
-id=1
-tests=`make list-tests`
+# build-tests.sh -- Attempt to build all binary tests and output TAP
+#
+# Run in the directory that contains the Makefile. Command line arguments are
+# treated as filters, and only tests with path names that match any of the
+# arguments are built.
 
-echo "# Building tests"
-echo "1..`echo "$tests" | wc -l`"
+# Parse command line for test filters
+PATTERNS=
+if [ "$#" -ge 1 ]; then
+  for PAT in "$@"; do
+    PATTERNS="$PATTERNS -e $PAT"
+  done
+  TESTS=$(make list-tests | grep $PATTERNS)
+else
+  TESTS=$(make list-tests)
+fi
 
-echo "$tests" | while read rule
+# If there are no tests, stop early
+if test -z "$TESTS"; then
+  echo "1..0 # SKIP No tests to build"
+  exit
+fi
+
+# Iterate over each test
+ID=1
+echo "# Building tests$PATTERNS"
+echo "1..$(echo "$TESTS" | wc -l)"
+echo "$TESTS" | while read RULE
 do
-  if make -q "$rule"; then
-    echo "ok $id $rule # skip: built and up to date"
+  if make -q "$RULE"; then
+    echo "ok $ID $RULE # skip: up to date"
   else
-    if output=`make "$rule"`; then
-      echo "ok $id $rule"
+    BUILD_DIR="$(dirname "$(dirname "$RULE")")/out"
+    BUILD_OUT="$BUILD_DIR/$(basename "$RULE").build"
+    mkdir -p "$BUILD_DIR"
+    if make "$RULE" >"$BUILD_OUT" 2>&1; then
+      echo "ok $ID $RULE"
     else
-      echo "not ok $id $rule"
-      echo "$output" | sed 's/^/	/'
+      echo "not ok $ID $RULE"
+      echo "	build output: $BUILD_OUT"
     fi
   fi
-  id=`expr 1 + $id`
+  ID=$(expr 1 + $ID)
 done
 
