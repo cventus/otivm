050cf2fe7cc047b55d81a5b9d38bcc1a63225e13
Christoffer Ventus <christoffer.ventus@futurice.com>
2015-09-13 23:48:51 +0300

    Add container_of and length_of macros to base

--- base/include/mem.h
+++ base/include/mem.h
@@ -1,3 +1,6 @@
 
+#define container_of(p,T,memb) (T *)((char *)(p) - offsetof(T,member))
+#define length_of(array) (sizeof (array) / sizeof 0[array])
+
 size_t align_to(size_t offset, size_t align);
 
