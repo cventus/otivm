a0c695a4bd96b60fc34615622c7ace451fa47eb8
Christoffer Ventus <christoffer.ventus@futurice.com>
2015-09-30 21:41:33 +0300

    Fix container_of() macro

--- base/include/mem.h
+++ base/include/mem.h
@@ -1,5 +1,5 @@
 
-#define container_of(p,T,memb) (T *)((char *)(p) - offsetof(T,member))
+#define container_of(p,T,member) (T *)((char *)(p) - offsetof(T,member))
 #define length_of(array) (sizeof (array) / sizeof 0[array])
 
 size_t align_to(size_t offset, size_t align);
