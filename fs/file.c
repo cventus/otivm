07c7157f556edcce6d4cd7dbccbd82fab6b84e04
Christoffer Ventus <christoffer.ventus@futurice.com>
2015-10-15 22:27:37 +0300

    Add module fs to interface with the file system

---
+++ fs/file.c
@@ -0,0 +1,68 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <base/wbuf.h>
+
+#include "include/file.h"
+
+char *relpath(char const *base, char const *path)
+{
+	char const *p;
+	struct wbuf buf;
+	size_t pathsz, prefixsz;
+
+	wbuf_init(&buf);
+	pathsz = strlen(path) + 1;
+	p = base ? strrchr(base, '/') : NULL;
+	if (p) {
+		prefixsz = (p - base) + 1;
+		if (wbuf_reserve(&buf, prefixsz + pathsz)) { return NULL; }
+		(void)wbuf_write(&buf, base, prefixsz);
+		(void)wbuf_write(&buf, path, pathsz);
+	} else {
+		if (!wbuf_write(&buf, path, pathsz)) { return NULL; }
+	}
+	return buf.begin;
+}
+
+FILE *open_relative(char const *rel, char const *filename, char const *mode)
+{
+	FILE *fp;
+	struct wbuf buf;
+	char const *p, *path;
+	size_t prefix, pathsize;
+
+	wbuf_init(&buf);
+	p = rel ? strrchr(rel, '/') : NULL;
+	if (p) {
+		prefix = (p - rel) + 1;
+		pathsize = strlen(filename) + 1;
+		if (wbuf_reserve(&buf, prefix + pathsize)) { return NULL; }
+		(void)wbuf_write(&buf, rel, prefix);
+		(void)wbuf_write(&buf, filename, pathsize);
+		path = buf.begin;
+	} else {
+		path = filename;
+	}
+	fp = fopen(path, mode);
+	wbuf_free(&buf);
+	return fp;
+}
+
+char *read_all(FILE *fp)
+{
+	char buffer[BUFSIZ];
+	struct wbuf buf;
+	size_t n;
+
+	wbuf_init(&buf);
+	while (n = fread(buffer, 1, sizeof buffer, fp), n > 0) {
+		if (!wbuf_write(&buf, buffer, n)) { goto error; }
+	}
+	if (ferror(fp) || !wbuf_write(&buf, "", 1)) { goto error; }
+	return buf.begin;
+
+error:	wbuf_free(&buf);
+	return NULL;
+}
+
---
+++ fs/module.mk
@@ -0,0 +1,2 @@
+MOD=base
+OUT=ar
