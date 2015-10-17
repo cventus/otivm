adc1166f7df4009b8267c9e79003e3815d0850b1
Christoffer Ventus <christoffer.ventus@futurice.com>
2015-10-17 14:19:01 +0300

    Update directory structure
    Move modules into the src/ directory and set the MODULE_DIR in config.mk

--- config.mk
+++ config.mk
@@ -1,5 +1,7 @@
 
-CFLAGS=-g -Wall -Wextra -pedantic -Wswitch-default -Wswitch-enum -Wshadow -std=c11 -Werror
 MODULES=ok base gm text wf fs rescache opengl
 TAGS=posix glx
+MODULE_DIR=src
+
+CFLAGS=-g -Wall -Wextra -pedantic -Wswitch-default -Wswitch-enum -Wshadow -std=c11 -Werror
 
