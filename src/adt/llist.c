42a481f9cd1dcb4413e6983f0d0c0f46fcdee1e9
Christoffer Ventus <christoffer.ventus@futurice.com>
2017-08-10 23:17:42 +0300

    Add intrusive list
    Intrusive structures, such as `struct ilist`, are supposed to be
    included in other structs. Generic functions are provided that operate
    on the included members directly, such as manipulating pointers, and
    something like the `container_of` macro should be used to go from a node
    to the containing structure.
    
    A `struct ilist` is an intrusive linked list node. Linked lists are
    either linear, which have a beginning and an end, or are circular.

---
+++ src/adt/llist.c
@@ -0,0 +1,103 @@
+#include <assert.h>
+#include <stddef.h>
+#include "include/ilist.h"
+
+void llist_init(struct ilist *list)
+{
+	assert(list != NULL);
+	list->prev = list->next = NULL;
+}
+
+int llist_singleton(struct ilist const *list)
+{
+	assert(list != NULL);
+	return list->prev == NULL && list->next == NULL;
+}
+
+void llist_insert_next(struct ilist *list, struct ilist *node)
+{
+	struct ilist *tail;
+	assert(list != NULL);
+	assert(node != NULL);
+
+	tail = list->next;
+
+	/* Set up node */
+	node->prev = list;
+	node->next = tail;
+
+	/* Change list */
+	if (tail) { tail->prev = node; }
+	list->next = node;
+}
+
+void llist_insert_prev(struct ilist *list, struct ilist *node)
+{
+	struct ilist *head;
+	assert(list != NULL);
+	assert(node != NULL);
+
+	head = list->prev;
+
+	/* Set up node */
+	node->prev = head;
+	node->next = list;
+
+	/* Update list */
+	if (head) { head->next = node; }
+	list->prev = node;
+}
+
+void llist_swap(struct ilist *a, struct ilist *b)
+{
+	struct ilist *p, *q;
+
+	assert(a != NULL);
+	assert(b != NULL);
+
+	if (a->next == b || b->next == a) {
+		if (a->next == b) {
+			p = a;
+			q = b;
+		} else {
+			p = b;
+			q = a;
+		}
+		if (p->prev) { p->prev->next = q; }
+		if (q->next) { q->next->prev = p; }
+		q->prev = p->prev;
+		p->next = q->next;
+		p->prev = q;
+		q->next = p;
+	} else if (a != b) {
+		if (a->prev) { a->prev->next = b; }
+		if (a->next) { a->next->prev = b; }
+		if (b->prev) { b->prev->next = a; }
+		if (b->next) { b->next->prev = a; }
+
+		p = a->next;
+		q = a->prev;
+		a->next = b->next;
+		a->prev = b->prev;
+		b->next = p;
+		b->prev = q;
+	}
+}
+
+void llist_remove(struct ilist *node)
+{
+	struct ilist *head, *tail;
+	assert(node != NULL);
+	head = node->prev;
+	tail = node->next;
+	if (head) { head->next = tail; }
+	if (tail) { tail->prev = head; }
+}
+
+size_t llist_length(struct ilist *node)
+{
+	struct ilist *p;
+	size_t n;
+	for (p = node, n = 0; p; p = p->next, n++) {}
+	return n;
+}
