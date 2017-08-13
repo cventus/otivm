16ef0a2b0ad7c498406d7473eb20165c2bdb4936
Christoffer Ventus <christoffer.ventus@futurice.com>
2017-08-13 21:43:49 +0300

    Add dynamic memory pool
    `mempool`, in contrast to `fixpool`, allocates more memory for more
    blocks on demand. For every N allocations it will allocate a new buffer
    and split it into blocks that can be allocated.
    
    Because pointer comparisons are strictly only defined for pointers
    within the same object or array, there is no attempt at detecting
    whether a pointer belongs to the pool when it is free'd or keeping track
    of which buffers have been emptied and could be released. Instead once a
    buffer has been allocated it will stay allocated until the pool itself
    is terminated. This matches allocation patterns where there are as many
    deallocations as allocations on average and no allocation spikes.

---
+++ src/base/mempool.c
@@ -0,0 +1,70 @@
+#include <stddef.h>
+#include <stdalign.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "include/wbuf.h"
+#include "include/fixpool.h"
+#include "include/mempool.h"
+
+void mempool_init(struct mempool *pool, size_t buffer_nmemb, size_t size)
+{
+	assert(size > 0);
+	assert(buffer_nmemb > 0);
+	assert(fixpool_aligned(size));
+	pool->nmemb = 0;
+	pool->buffer_nmemb = buffer_nmemb;
+	pool->size = size;
+	wbuf_init(&pool->buffers);
+}
+
+void mempool_term(struct mempool *pool)
+{
+	assert(pool != NULL);
+	void **p = pool->buffers.begin;
+	void **q = pool->buffers.end;
+	for (; p < q; p++) { free(*p); }
+	wbuf_term(&pool->buffers);
+	pool->nmemb = 0;
+	pool->buffer_nmemb = 0;
+	pool->size = 0;
+}
+
+size_t mempool_capacity(struct mempool *pool)
+{
+	assert(pool != NULL);
+	size_t bufsiz = pool->buffer_nmemb * pool->size;
+	return bufsiz * wbuf_nmemb(&pool->buffers, sizeof (void *));
+}
+
+void *mempool_alloc(struct mempool *pool)
+{
+	assert(pool != NULL);
+	if (!pool->free) {
+		if (wbuf_reserve(&pool->buffers, sizeof (void *))) {
+			return NULL;
+		}
+		struct fixpool fp;
+		size_t bufsiz = pool->buffer_nmemb * pool->size;
+		void *p = malloc(bufsiz);
+		if (!p) { return NULL; }
+		fixpool_init(&fp, p, pool->buffer_nmemb, pool->size);
+		(void)wbuf_write(&pool->buffers, &fp.buffer, sizeof fp.buffer);
+		pool->free = fp.free;
+	}
+	void *p = pool->free;
+	pool->free = *pool->free;
+	pool->nmemb++;
+	return p;
+}
+
+void mempool_free(struct mempool *pool, void *p)
+{
+	assert(pool != NULL);
+	if (p) {
+		void **q = p;
+		*q = pool->free;
+		pool->free = q;
+		pool->nmemb--;
+	}
+}
