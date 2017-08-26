bbe0011aae36b25abc431b6803c8ee3c99426073
Christoffer Ventus <christoffer.ventus@futurice.com>
2017-08-27 00:07:38 +0300

    Add demo with transform-animated shapes

--- src/xylo/module.mk
+++ src/xylo/module.mk
@@ -1,4 +1,4 @@
 # xylo 2D renderer
 OUT=archive
 MOD=base adt gm glapi glam
-TEST_MOD=ok
+TEST_MOD=ok tempo
--- src/xylo/test/render.c
+++ src/xylo/test/render.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
 #include <GL/gl.h>
 #include <ok/ok.h>
 
@@ -7,9 +10,12 @@
 #include <gm/matrix.h>
 #include <base/gbuf.h>
 
+#include <tempo/tempo.h>
+
 #include "../include/xylo.h"
 #include "../include/types.h"
 #include "../include/draw.h"
+#include "../include/tgraph.h"
 #include "../include/shape.h"
 #include "../private.h"
 
@@ -32,7 +38,7 @@ static struct xylo_shape test_shape = {
 			4,
 			(struct xylo_leg[]) {
 				{ { 0.0f, 0.5f }, { 0.5f, 0.5f }, 0.707106781f },
-				{ { 0.5f, 0.0f }, { 0.5f, 0.0f }, 1.f },
+				{ { 0.5f, 0.0f }, { 0.0f, 0.0f }, 1.f },
 				{ { 0.0f, -.5f }, { -.5f, -.5f }, 0.00001f },
 				{ { -.5f, 0.0f }, { -.5f, 0.5f }, 4.0f }
 			}
@@ -149,10 +155,141 @@ static int dlist_(struct gl_api *gl, struct gl_test *test)
 }
 static int dlist(void) { return run(dlist_); }
 
+static void copy_transform(struct xylo_tnode *tnode, struct xylo_dshape *shape)
+{
+	float const *t;
+	t = xylo_tnode_global(tnode);
+	(void)memcpy(shape->m22, t, 2 * sizeof *t);
+	(void)memcpy(shape->m22 + 2, t + 3, 2 * sizeof *t);
+	(void)memcpy(shape->pos, t + 6, sizeof shape->pos);
+}
+
+static void update(void *dest, void const *child, void const *parent)
+{
+	(void)m33mulf(dest, parent, child);
+}
+
+static int transformed_(struct gl_api *gl, struct gl_test *test)
+{
+	struct xylo *xylo;
+	struct xylo_glshape_set *set;
+	struct xylo_glshape const *shape;
+	float transform[16], proj[16], to_clip[16];
+	struct xylo_dlist dlist;
+	struct xylo_dshape a, b, c;
+	struct xylo_tgraph *tgraph;
+	struct xylo_tnode *root, *a_t[2], *b_t[1], *c_t[3];
+	struct pfclock *clk;
+	float id[9] = { 1.f, 0.f, 0.f, 0.f, 1.f, 0.f, 0.f, 0.f, 1.f };
+	float *p;
+	struct stopwatch sw;
+	double dt;
+
+	clk = pfclock_make();
+	if (!clk) { return -1; }
+	xylo = make_xylo(gl);
+	if (!xylo) { return -1; }
+	set = xylo_make_glshape_set(gl, 1, &test_shape);
+	if (!set) { return -1; }
+	shape = xylo_get_glshape(set, 0);
+	if (!shape) { return -1; }
+
+	/* create list nodes */
+	xylo_init_dshape(&a, shape);
+	xylo_init_dshape(&b, shape);
+	xylo_init_dshape(&c, shape);
+
+	/* create draw list */
+	xylo_init_dlist(&dlist);
+
+	xylo_dlist_append(&dlist, &a.header);
+	xylo_dlist_append(&dlist, &b.header);
+	xylo_dlist_append(&dlist, &c.header);
+
+	/* create transformation graph */
+	tgraph = xylo_make_tgraph(sizeof id);
+	root = xylo_make_tnode(tgraph, NULL, id);
+	a_t[0] = xylo_make_tnode(tgraph, root, id);
+	a_t[1] = xylo_make_tnode(tgraph, a_t[0], id);
+	b_t[0] = xylo_make_tnode(tgraph, a_t[0], id);
+	c_t[0] = xylo_make_tnode(tgraph, root, id);
+	c_t[1] = xylo_make_tnode(tgraph, c_t[0], id);
+	c_t[2] = xylo_make_tnode(tgraph, c_t[1], id);
+
+	p = xylo_tnode_local(b_t[0]);
+	p[0] = 0.22f;
+	p[4] = 0.33f;
+	p = xylo_tnode_local(c_t[1]);
+	p[0] = 0.3f;
+	p[4] = 0.3f;
+	p[6] = 0.3f;
+
+	/* setup matrices */
+	m44translatef(transform, 0.f, 0.f, -1.f);
+	m44perspectivef(proj, 1.570796327, 1.0f, 0.4f, 10.0f);
+	m44mulf(to_clip, proj, transform);
+
+	glClearColor(1.f, 1.f, 1.f, 1.f);
+
+	stopwatch_start(&sw, pfclock_usec(clk));
+	xylo_begin(xylo);
+	xylo_set_shape_set(xylo, set);
+	xylo_set_to_clip(xylo, to_clip);
+	do {
+		dt = stopwatch_elapsed(&sw, pfclock_usec(clk)) * 1.e-6;
+
+		/* animate nodes a bit*/
+		p = xylo_tnode_local(root);
+		p[6] = cos(dt) * 0.4f;
+		p[7] = sin(dt) * 0.4f;
+
+		p = xylo_tnode_local(a_t[0]);
+		m33rotzf(p, dt * -0.7f + 0.1f);
+
+		/* make a orbit b */
+		p = xylo_tnode_local(a_t[1]);
+		m33rotzf(p, -2.1f * dt);
+		p[0] *= 0.1f; p[1] *= 0.1f;
+		p[3] *= 0.1f; p[4] *= 0.1f;
+		p[6] = 0.3*sin(2.1f * dt);
+		p[7] = 0.3*cos(2.1f * dt);
+
+		p = xylo_tnode_local(c_t[0]);
+		m33rotzf(p, -dt);
+		p[6] = 0.2f + sin(dt * 0.73) * 0.1f;
+		p[7] = 0.2f + cos(dt * 0.73) * 0.1f;
+		p = xylo_tnode_local(c_t[2]);
+		m33rotzf(p, dt);
+
+		/* translate */
+		xylo_tgraph_transform(tgraph, update);
+		copy_transform(a_t[1], &a);
+		copy_transform(b_t[0], &b);
+		copy_transform(c_t[2], &c);
+
+		/* draw */
+		glClear(GL_COLOR_BUFFER_BIT);
+		xylo_draw(xylo, &dlist.header);
+		gl_test_swap_buffers(test);
+	} while (gl_test_poll_key(test) == 0);
+	xylo_end(xylo);
+
+	xylo_term_dlist(&dlist);
+	xylo_term_dshape(&a);
+	xylo_term_dshape(&b);
+	xylo_term_dshape(&c);
+	if (xylo_free_glshape_set(set, gl)) { return -1; }
+	free_xylo(xylo);
+	pfclock_free(clk);
+	return 0;
+}
+static int transformed(void) { return run(transformed_); }
+
 struct test const tests[] = {
 	{ creation, "create xylo renderer" },
 	{ draw, "simple render" },
 	{ dlist, "render draw list" },
+	{ transformed, "render items with tgraph" },
 
 	{ NULL, NULL }
 };
