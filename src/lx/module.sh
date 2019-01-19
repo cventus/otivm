4e43fe0353144a81e7bc25358c46716094815c61
Christoffer Ventus <christoffer.ventus@gmail.com>
2019-01-19 22:09:21 +0200

    Rename names of tests

--- src/lx/module.sh
+++ src/lx/module.sh
@@ -1,4 +1,4 @@
 define_ok_test test/equals.c
-define_ok_test test/list-array.c
-define_ok_test test/list-pairs.c
+define_ok_test test/adjacent-list.c
+define_ok_test test/linked-list.c
 define_ok_test test/cons.c
